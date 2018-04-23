9faa21937bfe50e6dc95e657b45f896353c1c837
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/SourceReports/XtraReport1.designer.cs
@@ -0,0 +1,153 @@
+namespace MasterDetailReportLoadedFromRepx
+{
+    partial class XtraReport1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.xrSubreport1 = new DevExpress.XtraReports.UI.XRSubreport();
+            this.xrPictureBox1 = new DevExpress.XtraReports.UI.XRPictureBox();
+            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            this.PageHeader = new DevExpress.XtraReports.UI.PageHeaderBand();
+            this.PageFooter = new DevExpress.XtraReports.UI.PageFooterBand();
+            this.nwindDataSet1 = new MasterDetailReportLoadedFromRepx.nwindDataSet();
+            this.categoriesTableAdapter = new MasterDetailReportLoadedFromRepx.nwindDataSetTableAdapters.CategoriesTableAdapter();
+            this.topMarginBand1 = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.bottomMarginBand1 = new DevExpress.XtraReports.UI.BottomMarginBand();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrSubreport1,
+            this.xrPictureBox1,
+            this.xrLabel2,
+            this.xrLabel1});
+            this.Detail.HeightF = 190F;
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // xrSubreport1
+            // 
+            this.xrSubreport1.LocationFloat = new DevExpress.Utils.PointFloat(67F, 83F);
+            this.xrSubreport1.Name = "xrSubreport1";
+            this.xrSubreport1.SizeF = new System.Drawing.SizeF(433F, 58F);
+            // 
+            // xrPictureBox1
+            // 
+            this.xrPictureBox1.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Image", null, "Categories.Picture")});
+            this.xrPictureBox1.LocationFloat = new DevExpress.Utils.PointFloat(333F, 8F);
+            this.xrPictureBox1.Name = "xrPictureBox1";
+            this.xrPictureBox1.SizeF = new System.Drawing.SizeF(158F, 67F);
+            // 
+            // xrLabel2
+            // 
+            this.xrLabel2.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "Categories.Description")});
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(67F, 42F);
+            this.xrLabel2.Name = "xrLabel2";
+            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
+            this.xrLabel2.SizeF = new System.Drawing.SizeF(192F, 33F);
+            this.xrLabel2.Text = "xrLabel2";
+            // 
+            // xrLabel1
+            // 
+            this.xrLabel1.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "Categories.CategoryName")});
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(67F, 8F);
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(192F, 25F);
+            this.xrLabel1.Text = "xrLabel1";
+            // 
+            // PageHeader
+            // 
+            this.PageHeader.HeightF = 30F;
+            this.PageHeader.Name = "PageHeader";
+            this.PageHeader.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // PageFooter
+            // 
+            this.PageFooter.HeightF = 30F;
+            this.PageFooter.Name = "PageFooter";
+            this.PageFooter.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // nwindDataSet1
+            // 
+            this.nwindDataSet1.DataSetName = "nwindDataSet";
+            this.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // categoriesTableAdapter
+            // 
+            this.categoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // topMarginBand1
+            // 
+            this.topMarginBand1.Name = "topMarginBand1";
+            // 
+            // bottomMarginBand1
+            // 
+            this.bottomMarginBand1.Name = "bottomMarginBand1";
+            // 
+            // XtraReport1
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.PageHeader,
+            this.PageFooter,
+            this.topMarginBand1,
+            this.bottomMarginBand1});
+            this.DataAdapter = this.categoriesTableAdapter;
+            this.DataMember = "Categories";
+            this.DataSource = this.nwindDataSet1;
+            this.Version = "12.2";
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.PageHeaderBand PageHeader;
+        private DevExpress.XtraReports.UI.PageFooterBand PageFooter;
+        private nwindDataSet nwindDataSet1;
+        private MasterDetailReportLoadedFromRepx.nwindDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
+        private DevExpress.XtraReports.UI.XRPictureBox xrPictureBox1;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel2;
+        private DevExpress.XtraReports.UI.XRSubreport xrSubreport1;
+        private DevExpress.XtraReports.UI.TopMarginBand topMarginBand1;
+        private DevExpress.XtraReports.UI.BottomMarginBand bottomMarginBand1;
+    }
+}
---
+++ VB/WindowsFormsApplication1/SourceReports/XtraReport2.designer.vb
@@ -0,0 +1,121 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace MasterDetailReportLoadedFromRepx
+	Partial Public Class XtraReport2
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.nwindDataSet11 = New MasterDetailReportLoadedFromRepx.nwindDataSet1()
+			Me.productsTableAdapter = New MasterDetailReportLoadedFromRepx.nwindDataSet1TableAdapters.ProductsTableAdapter()
+			Me.topMarginBand1 = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.bottomMarginBand1 = New DevExpress.XtraReports.UI.BottomMarginBand()
+			CType(Me.nwindDataSet11, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2, Me.xrLabel1})
+			Me.Detail.HeightF = 40F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrLabel2
+			' 
+			Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitPrice")})
+			Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(325F, 8F)
+			Me.xrLabel2.Name = "xrLabel2"
+			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel2.SizeF = New System.Drawing.SizeF(100F, 25F)
+			Me.xrLabel2.Text = "xrLabel2"
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.ProductName")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(67F, 8F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(133F, 25F)
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.HeightF = 30F
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.HeightF = 30F
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' nwindDataSet11
+			' 
+			Me.nwindDataSet11.DataSetName = "nwindDataSet1"
+			Me.nwindDataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' productsTableAdapter
+			' 
+			Me.productsTableAdapter.ClearBeforeFill = True
+			' 
+			' topMarginBand1
+			' 
+			Me.topMarginBand1.Name = "topMarginBand1"
+			' 
+			' bottomMarginBand1
+			' 
+			Me.bottomMarginBand1.Name = "bottomMarginBand1"
+			' 
+			' XtraReport2
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter, Me.topMarginBand1, Me.bottomMarginBand1})
+			Me.DataAdapter = Me.productsTableAdapter
+			Me.DataMember = "Products"
+			Me.DataSource = Me.nwindDataSet11
+			Me.Version = "12.2"
+			CType(Me.nwindDataSet11, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private nwindDataSet11 As nwindDataSet1
+		Private productsTableAdapter As MasterDetailReportLoadedFromRepx.nwindDataSet1TableAdapters.ProductsTableAdapter
+		Private topMarginBand1 As DevExpress.XtraReports.UI.TopMarginBand
+		Private bottomMarginBand1 As DevExpress.XtraReports.UI.BottomMarginBand
+	End Class
+End Namespace
